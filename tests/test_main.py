b46d2e4da810bb5d79a20884aaf5e100cb80e508
diemeise <67091927+diemeise@users.noreply.github.com>
2025-01-11 11:50:30 +0000

    disable traces in unittests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,15 @@
 from note_api.model import CreateNoteRequest, Note
 from note_api.backends import MemoryBackend
 from note_api.main import create_note, get_note, get_notes
+import pytest
+from opentelemetry import trace
+from opentelemetry.sdk.trace import NoOpTracerProvider
 
+#In Unittests sollten keine Traces an die Produktivumgebung gesendet werden.
+#Da keine anderen Umgebungen definiert sind werden Traces "deaktiviert"
+@pytest.fixture(scope="session", autouse=True)
+def disable_opentelemetry():
+    trace.set_tracer_provider(NoOpTracerProvider);
 
 def test_save_and_get_item():
     backend = MemoryBackend()
